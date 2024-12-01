73d1eecf430f333a5a439e3c269df725822b1066
manideep412m <manideep412m@gmail.com>
2024-12-01 22:10:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,30 @@ resource"azurerm_resource_group" "cloudlearning" {
   name  = "cloudlearning3"
   location = "UKSouth"
 }
+
+
+
+resource "azurerm_container_group" "bharathcontainerinstance" {
+  name                = "bharathcontainerinstance"
+  location            = azurerm_resource_group.cloudlearning.location
+  resource_group_name = azurerm_resource_group.cloudlearning.name
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world"
+    image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}

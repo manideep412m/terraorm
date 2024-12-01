2f98f182fcfdfd3ef298d7f03ccc58aa4b09da4c
manideep412m <manideep412m@gmail.com>
2024-12-01 22:42:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,18 +3,17 @@ provider "azurerm"{
   resource_provider_registrations = "none"
   subscription_id = "94b32def-28a7-475d-bbe4-bfa09615697a"
 }
-# Define the resource group
-resource"azurerm_resource_group" "cloudlearning" {
-  name  = "cloudlearning3"
-  location = "UKSouth"
+data "azurerm_resource_group" "bharathrs" {
+  name = "bharathrs"
 }
+
 
 
 
 resource "azurerm_container_group" "bharathcontainerinstance" {
   name                = "bharathcontainerinstance"
-  location            = azurerm_resource_group.cloudlearning.location
-  resource_group_name = azurerm_resource_group.cloudlearning.name
+  location            = azurerm_resource_group.bharathrs.location
+  resource_group_name = azurerm_resource_group.bharathrs.name
   ip_address_type     = "Public"
   dns_name_label      = "aci-label"
   os_type             = "Linux"
